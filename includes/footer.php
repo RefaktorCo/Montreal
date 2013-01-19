347cb6f0b4af33b8b3b20e5ca46f8459abbe69f9
refaktorco <refaktor.co@gmail.com>
2013-01-18 17:50:09 -0800

    -Footer block region markup

--- includes/footer.php
+++ includes/footer.php
@@ -6,76 +6,51 @@ function montreal_footer($page){
   <!-- FOOTER -->
 <footer class="black">
 <div class="container">
+
+  <?php if (theme_get_setting('enable_primary_footer') == '1') { ?>
 	<div class="row bigtoppadding">
 	
-		<!-- FIRST WIDGET -->
 		<div class="three columns">
-			<h5 class="whitetext light">Contact Us Here.</h5>
-			<p class="greytext midmargin meta">
-				 786 Some Place Nice, Rd Nice<br>
-				 State, Big Country 7860
-			</p>
-		</div>
-		<!--SECOND WIDGET -->
-		<div class="three columns">
-			<h5 class="whitetext light">Or Here.</h5>
-			<p class="greytext midmargin meta">
-				 p <span>(786) 9876 543 210</span><br>
-				 e <a href="mailto:" class="whitetext">contact@youragency.com</a>
-			</p>
-		</div>
-		<!-- THIRD WIDGET -->
-		<div class="three columns">
-			<h5 class="whitetext light">Project Planner.</h5>
-			<p class="greytext midmargin meta">
-				 We have prepared a <a href="/projectplanner.html" class="whitetext">project planner</a> to get to know your project better.
-			</p>
-		</div>
-		<!-- FOURTH WIDGET -->
-		<div class="three columns newsletter">
-			<h5 class="whitetext light">Newsletter.</h5>
-			<p class="smallfont">
-				 Email updates.
-			</p>
-			<form id="newsletter" action="#" method="post">
-				<dl class="field row">
-					<dt class="text"><input type="text" placeholder="Your Email..."/></dt>
-					<dd class="msg"><span class="caret"></span>You filled this out wrong.</dd>
-					</dl>
-					<input type="submit" name="Submit" value="Submit" class="submit alpha four columns">
-				
-			</form>
-		</div>
+      <?php if(!$page['footer_1']) {?>
+      <h2>Footer Block 1</h2>
+      <?php } else { print render($page['footer_1']); }?>   
+    </div> 
+
+    <div class="three columns">
+      <?php if(!$page['footer_2']) {?>
+      <h2>Footer Block 2</h2>
+      <?php } else { print render($page['footer_2']); }?>  
+    </div>
+    
+    <div class="three columns">
+      <?php if(!$page['footer_3']) {?>
+      <h2>Footer Block 3</h2>
+      <?php } else { print render($page['footer_3']); }?>  
+    </div>
+    
+    <div class="three columns">
+      <?php if(!$page['footer_4']) {?>
+      <h2>Footer Block 4</h2>
+      <?php } else { print render($page['footer_4']); }?>      
+    </div>
 	</div>
 	<!-- END TOP ROW -->
+	<?php endif; ?>
 	
+	<?php if (theme_get_setting('enable_secondary_footer') == '1') { ?>
 	<!-- GREY HORIZONTAL -->
 	<div class="greyhorizontal midmargin">
 	</div>
 	
 	<div class="row">
 	
-	<!-- COPYRIGHT -->
-		<div class="four columns">
-			<p class="greytext meta">
-				 © 2012 <a href="#" title="Website Design, Graphic Design, Applications &amp; Development" class="greytext meta">Montreal RSPNSV HTML</a>
-			</p>
-		</div>
-		
-		<!-- NAVIGATION -->
-		<div class="five columns">
-			<p class="meta">
-				<a href="#" class="greytext">Home</a> &nbsp; / &nbsp; <a href="/about.html" class="greytext">About</a> &nbsp; / &nbsp; <a href="/portfolio.html" class="greytext">Portfolio</a> &nbsp; / &nbsp; <a href="/blog.html" class="greytext">Blog</a> &nbsp; / &nbsp; <a href="/contact.html" class="greytext">Contact</a>
-			</p>
-		</div>
-		
-		<!-- LINKS -->
-		<div class="three columns right">
-			<p class="meta">
-				<a href="#" class="greytext">Facebook</a> / &nbsp; <a href="#" class="greytext">Twitter</a> / &nbsp; <a href="#" class="greytext">Behance</a>
-			</p>
-		</div>
-	</div>
+	 <div class="twelve columns"> 
+    <?php if(!$page['footer_full']) {?>    
+    <h6 class="after_footer"><?php echo theme_get_setting('secondary_footer_text'); ?></h6>
+    <?php } else { print render($page['footer_full']); }?>  
+  </div> 	
+  </div>
+  <?php endif; ?>
 </div>
 </footer>
     
