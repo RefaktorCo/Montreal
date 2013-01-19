08edd2853d23abdaec07fe268f17877aee711ed1
refaktorco <refaktor.co@gmail.com>
2013-01-18 16:51:47 -0800

    -Couple modifications to padding divs

--- includes/front-page.php
+++ includes/front-page.php
@@ -19,7 +19,7 @@ PLEASE REMEMBER: THE BACKGROUND HAS LOW OPACITY TO MAKE SLIDESHOW VISIBLE.-->
 
 <?php if (theme_get_setting('enable_recent_projects') == '1') : ?>	
 <!-- THICK STRIPE BORDER -->
-<div class="smallpadding">
+<div class="smallpadding" style="background: url('<?php echo $root; ?>/img/stripes.png');">
 </div>
 <!-- PORTFOLIO CONTAINER -->
 <div class="container white bigpadding">
@@ -49,7 +49,7 @@ PLEASE REMEMBER: THE BACKGROUND HAS LOW OPACITY TO MAKE SLIDESHOW VISIBLE.-->
 
 <?php if (theme_get_setting('enable_highlight') == '1') : ?>
 <!-- THICK STRIPE BORDER -->
-<div class="smallpadding">
+<div class="smallpadding" style="background: url('<?php echo $root; ?>/img/stripes.png');" >
 </div>
 <!-- QUOTE CONTAINER WITH STRIPE BACKGROUND AND SLIDESHOW IN THE BACK -->
 <div class="container largepadding_back">
--- templates/page--portfolio-block.tpl.php
+++ templates/page--portfolio-block.tpl.php
@@ -1,6 +1,6 @@
 <?php global $root; montreal_header($page);?>
  <!-- PORTFOLIO ITEM CONTAINER / GRIDDER -->
- <div class="container nopadding bigtoppadding gridder">       
+ <div class="container smallpadding bigtoppadding gridder">       
         
     <?php print render($title_prefix); ?>
     <?php print render($title_suffix); ?>
