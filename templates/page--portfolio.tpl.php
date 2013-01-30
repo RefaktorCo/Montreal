b835df2ebda65847230ae1bbfd7748e03afcba82
refaktorco <refaktor.co@gmail.com>
2013-01-30 12:09:38 -0800

    -Had to make a tweak to the basic portfolio page appearance.

--- templates/page--portfolio.tpl.php
+++ templates/page--portfolio.tpl.php
@@ -1,30 +1,19 @@
 <?php global $root; montreal_header($page);?>
       
-	<div class="container bigpadding" style="background:url(<?php echo $root;?>/img/stripes.png);">
+	<div class="container bigtoppadding midtoppadding" style="background:url(<?php echo $root;?>/img/stripes.png);">
+		<section class="row midbottompadding bigtoppadding">
+		<h2 class="black whitetext bold leftpadding rightpadding">SOME OF OUR WORK</h2>
+		</section>
   </div>
 	  
   <div class="container white bigpadding">
-	  <section class="row">
-	  	<h3 class="blacktext bold midbottommargin center">OUR RECENT WORK</h3>
-			<div class="five columns alpha centered blackhorizontal">
-			</div>
-			<div class="four columns alpha centered midtopmargin">
-				<p class="center meta">
-					We have worked on some amazing project with some great clients.
-				</p>
-			</div>
-	  </section>
-
-      <!-- Begin Isotope filters -->
-           
-      <section class="row bigtoppadding portfolio_item">
-        <!-- Begin Isotope content -->
-        
+	    
+      <section class="row portfolio_item">
+      
           <?php print render($title_prefix); ?>
           <?php print render($title_suffix); ?>
           <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
           <?php print render($page['content']); ?>
-        
         
       </section>
   </div>
