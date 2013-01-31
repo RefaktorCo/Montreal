f008f8be3a5f9ae56774eccaf748450501cace18
refaktorco <refaktor.co@gmail.com>
2013-01-31 15:28:55 -0800

    -All template files cleaned up.

--- templates/page--portfolio-block.tpl.php
+++ templates/page--portfolio-block.tpl.php
@@ -2,10 +2,10 @@
  <!-- PORTFOLIO ITEM CONTAINER / GRIDDER -->
  <div class="container smallpadding bigtoppadding gridder">       
         
-    <?php print render($title_prefix); ?>
-    <?php print render($title_suffix); ?>
-    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-    <?php print render($page['content']); ?>
+  <?php print render($title_prefix); ?>
+  <?php print render($title_suffix); ?>
+  <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+  <?php print render($page['content']); ?>
         
  </div>
  <div style="clear:both"></div>
